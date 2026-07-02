7b05eae6c28fd9b449d2483fe93bf33c0d7bc02c
sugitha kalyani <sugithad91@gmail.com>
2026-07-02 15:32:18 +0300

    Add vercel routing configuration

--- charity_project/charity_project/wsgi.py
+++ charity_project/charity_project/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'charity_project.settings')
+app = application
 
-application = get_wsgi_application()
